c3c75d34140c630dd328e79009c6fa795d883941
Gabriele Crestanello <gabricre01@gmail.com>
2022-10-24 17:40:34 +0200

    TG-56 #in-progress

---
+++ main.go
@@ -0,0 +1,65 @@
+// @title       Birdazzone API
+// @version     0.1
+// @description API implementation for the Software Engineering (90106) project for the academic year 2022/23 at the University of Bologna.
+
+// @license.name GNU AFFERO GENERAL PUBLIC LICENSE
+// @license.url  https://www.gnu.org/licenses/agpl-3.0.en.html
+
+// @host     localhost:8080
+// @BasePath /api/v1
+
+package main
+
+import (
+	"net/http"
+	"os"
+
+	_ "git.hjkl.gq/team13/birdazzone-api/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+const EnvHost = "HOST"
+const EnvPort = "PORT"
+const FallbackHost = "0.0.0.0"
+const FallbackPort = "8080"
+const BasePath = "/api/v1"
+
+func lookupEnvWithFallback(key string, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	if ok {
+		return val
+	}
+	return fallback
+}
+
+func address() string {
+	return lookupEnvWithFallback(EnvHost, FallbackHost) + ":" +
+		lookupEnvWithFallback(EnvPort, FallbackPort)
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET(BasePath+"/hello", helloWorld)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(address())
+}
+func staticMessage(c *gin.Context, msg string) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": msg,
+	})
+}
+
+// helloWorld godoc
+// @Summary     Test your connection to birdazzone API
+// @Tags        test
+// @Produce     json
+// @Success     200
+// @Router      /hello [get]
+func helloWorld(ctx *gin.Context) {
+	staticMessage(ctx, "Hi! You've successfully connected to Birdazzone API.")
+}
