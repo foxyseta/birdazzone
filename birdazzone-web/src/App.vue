bd57b90f838f75b5ed7a39d3c8ba593532c008b1
mattia <m@tt.ia>
2022-10-30 09:25:47 +0100

    chore: project structure

--- birdazzone-web/src/App.vue
+++ birdazzone-web/src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import { getHelloBird } from './api/api-repository';
-
-const test = async ()=>{await getHelloBird()}
 </script>
 
 <template>
-    <button @click="test">GET</button>
 </template>
--- birdazzone-web/src/api/api-repository.ts
+++ birdazzone-web/src/api/api-repository.ts
@@ -1,8 +1,8 @@
-import { ApiManager } from "./api"
+import { ApiManager, ApiResponse } from "./api"
+import type HelloBird from "./interfaces/hello-bird"
 
-interface HelloBird{
-    message: string
-}
+const _BASE_URL = "http://localhost:8080/api/v1"
+const _HELLO = "/hello"
 
-export const getHelloBird =  () =>
-    ApiManager.get<HelloBird>("http://localhost:8080/api/v1/hello")
+export const getHelloBird = (): Promise<ApiResponse<HelloBird>> =>
+  ApiManager.get<HelloBird>(_BASE_URL + _HELLO)
