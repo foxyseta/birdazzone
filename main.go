a41f79ed0033c6104e80be9ec14f2cb242fd00e9
mattia <m@tt.ia>
2022-10-30 09:17:04 +0100

    fix: cors middleware

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func address() string {
 func main() {
 	//gin + API
 	r := gin.Default()
+  r.Use(corsMiddleware())
 	v1 := r.Group("/api/v1")
 	v1.GET("/hello", helloWorld)
 
@@ -70,4 +71,21 @@ func staticMessage(c *gin.Context, msg string) {
 func helloWorld(ctx *gin.Context) {
 	staticMessage(ctx, "Hi! You've successfully connected to Birdazzone API.")
 
+}
+
+// TODO 
+func corsMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
