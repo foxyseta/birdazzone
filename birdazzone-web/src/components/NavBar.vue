7af71b597be7d2e8f606eed92e30d520ea0a29ab
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-10-31 11:42:09 +0100

    Some fixes

--- birdazzone-web/src/components/NavBar.vue
+++ birdazzone-web/src/components/NavBar.vue
@@ -4,8 +4,8 @@
         <div class="relative flex h-16 items-center justify-between">
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div class="flex flex-shrink-0 items-center">
-              <img class="block h-8 w-auto lg:hidden" src="../../public/logoFede.jpeg" alt="Your Company" />
-              <img class="hidden h-8 w-auto lg:block" src="../../public/logoFede.jpeg" alt="Your Company" />
+              <img class="block h-8 w-auto lg:hidden" src="/logoFede.jpeg" alt="Your Company" />
+              <img class="hidden h-8 w-auto lg:block" src="/logoFede.jpeg" alt="Your Company" />
             </div>
           </div>
         </div>
@@ -13,7 +13,7 @@
     </Disclosure>
   </template>
   
-  <script setup>
+  <script setup lang=ts>
   import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
   import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
   </script>
