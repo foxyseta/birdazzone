97e7a3ce9d1d2b603636f4dfa24a99ffb3a6853f
mattia <m@tt.ia>
2022-10-31 13:41:24 +0100

    feat: first prototype of tweet list

--- birdazzone-web/src/App.vue
+++ birdazzone-web/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloBird from './components/HelloBird.vue' 
+import HelloBird from './components/HelloBird.vue'
+import TweetList from './components/TweetList.vue' 
 </script>
 
 <template>
   <HelloBird />
+  <TweetList />
 </template>
