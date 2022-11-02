eb9e0a28194600cc13dbfbb80cc2bb4a4f6f16bb
mattia <m@tt.ia>
2022-11-02 14:30:18 +0100

    feat: clean project

--- birdazzone-web/src/App.vue
+++ birdazzone-web/src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import HelloBird from './components/HelloBird.vue'
-import TweetList from './components/TweetList.vue' 
 import NavBar from "./components/NavBar.vue"
 </script>
 
 <template>
   <NavBar />
-  <HelloBird />
-  <TweetList />
 </template>
