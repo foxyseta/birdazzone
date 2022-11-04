0e468b81dd117b760f150675170432272f7d077b
Gabriele Crestanello <gabriele.crestanello@studio.unibo.it>
2022-11-04 09:42:27 +0100

    TG-77 #in-progress 30' fix serverRun()

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func address() string {
 func runServer() {
 	//gin + API
 	r := gin.Default()
-  r.Use(corsMiddleware())
+	r.Use(corsMiddleware())
 	v1 := r.Group("/api/v1")
 	v1.GET("/hello", helloWorld)
 
@@ -58,7 +58,7 @@ func runServer() {
 }
 
 func main() {
-  runServer()
+	runServer()
 }
 
 func staticMessage(c *gin.Context, msg string) {
@@ -78,19 +78,19 @@ func helloWorld(ctx *gin.Context) {
 
 }
 
-// TODO 
+// TODO
 func corsMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,49 +1,56 @@
 package main
 
-import(
-  "net"
-  "os"
-  "strconv"
-  "strings"
-  "testing"
+import (
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
 )
 
 func TestLookupEnvWithFallbackSuccess(t *testing.T) {
-  const VarName = "FOOD"
-  const VarValue = "PIZZA"
-  os.Setenv(VarName,VarValue)
-  if lookupEnvWithFallback(VarName, "") != os.Getenv(VarName) {
-    t.Fatal("Wrong variable value")
-  }
+	const VarName = "FOOD"
+	const VarValue = "PIZZA"
+	os.Setenv(VarName, VarValue)
+	if lookupEnvWithFallback(VarName, "") != os.Getenv(VarName) {
+		t.Fatal("Wrong variable value")
+	}
 }
 
 func TestLookupEnvWithFallbackFailure(t *testing.T) {
-  const VarName = "DRINK"
-  const Fallback = "WATER"
-  os.Unsetenv(VarName)
-  if lookupEnvWithFallback(VarName, Fallback) != Fallback {
-    t.Fatal("Wrong variable value")
-  }
+	const VarName = "DRINK"
+	const Fallback = "WATER"
+	os.Unsetenv(VarName)
+	if lookupEnvWithFallback(VarName, Fallback) != Fallback {
+		t.Fatal("Wrong variable value")
+	}
 }
 
 func TestAddress(t *testing.T) {
-  tokens := strings.Split(address(), ":") 
-  if len(tokens) != 2 {
-    t.Fatal("Wrong number of colons")
-  }
-  if net.ParseIP(tokens[0]) == nil {
-    t.Fatal(tokens[0] + " is no valid host")
-  }
-  i, err := strconv.Atoi(tokens[1])
-  if err != nil {
-    t.Fatal(tokens[1] + " is no integer number")
-  }
-  if i < 0 {
-    t.Fatalf("Negative port number %d", i)
-  }
+	tokens := strings.Split(address(), ":")
+	if len(tokens) != 2 {
+		t.Fatal("Wrong number of colons")
+	}
+	if net.ParseIP(tokens[0]) == nil {
+		t.Fatal(tokens[0] + " is no valid host")
+	}
+	i, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		t.Fatal(tokens[1] + " is no integer number")
+	}
+	if i < 0 {
+		t.Fatalf("Negative port number %d", i)
+	}
 }
 
 func TestMain(m *testing.M) {
-  runServer()
-  os.Exit(m.Run())
+	sttyArgs := syscall.ProcAttr{
+		Dir:   "",
+		Env:   []string{},
+		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
+		Sys:   nil,
+	}
+	syscall.ForkExec("go", []string{"run", "."}, &sttyArgs)
+	os.Exit(m.Run())
 }
