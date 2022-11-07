a3a331cd5be79a3db4a2c1f9785f801dfd1684c9
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-11-07 23:59:14 +0100

    Add alt attribute to images

--- birdazzone-web/src/views/NotFoundView.vue
+++ birdazzone-web/src/views/NotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center">
-    <img src="/mascotte.jpeg" />
+    <img src="/mascotte.jpeg" alt="Birdazzone's mascotte"/>
     <h1 class="font-black text-4xl text-white">Ups, not found!</h1>
   </div>
 </template>
