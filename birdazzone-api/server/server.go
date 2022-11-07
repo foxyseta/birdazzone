3ab5ceca588a6f3b12b065f7af812e981f781410
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-11-07 19:44:25 +0100

    WIP fix tests

--- birdazzone-api/server/server.go
+++ birdazzone-api/server/server.go
@@ -21,35 +21,28 @@ func lookupEnvWithFallback(key string, fallback string) string {
 	return fallback
 }
 
-// TODO: CORS headers (Mattia)
-func corsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
+func corsMiddleware(c *gin.Context)  {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-		if c.Request.Method == "OPTIONS" {
+		if c.Request != nil && c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
 
 		c.Next()
-	}
 }
 
-func address() string {
+func Address() string {
 	return lookupEnvWithFallback(EnvHost, FallbackHost) + ":" +
 		lookupEnvWithFallback(EnvPort, FallbackPort)
 }
 
 func CreateServer() *gin.Engine {
 	r := gin.Default()
-	r.Use(corsMiddleware())
+	r.Use(corsMiddleware)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return r
-}
-
-func Run(s *gin.Engine) {
-	s.Run(address())
 }
