386ce2a8f5dfccde2869cbf873de5b4fddf4038b
Gabriele Crestanello <gabriele.crestanello@studio.unibo.it>
2022-11-08 19:47:46 +0100

    TG-65 #ready-for-test 50' get ghigliottina solution from Twitter

--- birdazzone-api/twitter/twitter_test.go
+++ birdazzone-api/twitter/twitter_test.go
@@ -7,8 +7,8 @@ import (
 	"git.hjkl.gq/team13/birdazzone-api/util"
 )
 
-func TestGetTweetsQuery(t *testing.T) {
-	res := getTweetsQuery("kalulu")
+func TestGetTweetsFromQuery(t *testing.T) {
+	res := getTweetsFromQuery("kalulu")
 	if res.Code != 200 {
 		t.Fatalf("Cannot retrieve GET request from Twitter")
 	}
@@ -17,7 +17,7 @@ func TestGetTweetsQuery(t *testing.T) {
 func TestReturnTweetQuery(t *testing.T) {
 	body := `{"text":"TEST STRING","user":"@myusername"}`
 	//should return a json + Code 200
-	returnTweetQuery(util.GetTestingGinContext(), queryResponse{&body, 200, "200 OK"})
+	returnTweetQuery(util.GetTestingGinContext(), QueryResponse{&body, 200, "200 OK"})
 	if util.GetTestingResponseRecorder().Code != http.StatusOK {
 		t.Fatalf("Expected to get status %d but instead got %d", http.StatusOK,
 			util.GetTestingResponseRecorder().Code)
