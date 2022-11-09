5db75f7d4100b0523bfcba5ed3a37d4509985980
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-11-09 19:37:22 +0100

    TG-87 #closed 15'

--- tvgames/interface.go
+++ tvgames/interface.go
@@ -1,6 +1,10 @@
 package tvgames
 
-type TvGame interface {
+import "git.hjkl.gq/team13/birdazzone-api/model"
+
+type GameTracker interface {
+  // The game being tracked
+  Game() model.Game
 	// the hashtag used by the users playing this TV game
 	Hashtags() string
 	// the solution to the last instance of this TV game
