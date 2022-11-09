38cb7d61c913a284be6477ccadc90236dbca5938
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-11-09 19:44:51 +0100

    Format tvgames/interface

--- birdazzone-api/tvgames/interface.go
+++ birdazzone-api/tvgames/interface.go
@@ -3,8 +3,8 @@ package tvgames
 import "git.hjkl.gq/team13/birdazzone-api/model"
 
 type GameTracker interface {
-  // The game being tracked
-  Game() model.Game
+	// The game being tracked
+	Game() model.Game
 	// the hashtag used by the users playing this TV game
 	Hashtags() string
 	// the solution to the last instance of this TV game
