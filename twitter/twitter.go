dfb0da30673b13daf819618027779ebebcb5d611
Gabriele Crestanello <gabriele.crestanello@studio.unibo.it>
2022-11-09 15:32:12 +0100

    TG-86 #in-progress 100' solution fetching + API /:game/solution + cleaner twitter module + some tests

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -1,159 +1,11 @@
 package twitter
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"fmt"
 	"net/http"
-	"net/url"
-	"regexp"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
-
-const BearerToken = "AAAAAAAAAAAAAAAAAAAAAE4higEAAAAAIAkazaLrT4LHjJx2XFPsdVzEPe8%3DE7HE9wBq5B5b0m4F8uGmcslObTmQFccb9gppULjUwTNBGj1Td3"
-
-type QueryResponse struct {
-	Body   *string
-	Code   int
-	Status string
-}
-
-// Basic user profile data
-type UIDLookup struct {
-	Data struct {
-		ID       string `json:"id"`
-		Name     string `json:"name"`
-		Username string `json:"username"`
-	} `json:"data"`
-}
-
-// List of tweets from a single profile
-type ProfileTweets struct {
-	Data []struct {
-		EditHistoryTweetIds []string `json:"edit_history_tweet_ids"`
-		ID                  string   `json:"id"`
-		Text                string   `json:"text"`
-	} `json:"data"`
-	Meta struct {
-		NextToken   string `json:"next_token"`
-		ResultCount int    `json:"result_count"`
-		NewestID    string `json:"newest_id"`
-		OldestID    string `json:"oldest_id"`
-	} `json:"meta"`
-}
-
-func getRequest(url string) (*http.Response, error) {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Authorization", "Bearer "+BearerToken)
-	resp, err := client.Do(req)
-	return resp, err
-}
-
-func getTweetsFromQuery(query string) QueryResponse {
-	max_results := "10"
-	query = url.QueryEscape(query)
-	resp, err := getRequest("https://api.twitter.com/2/tweets/search/recent?query=" + query + "&max_results=" + max_results + "&tweet.fields=public_metrics&expansions=geo.place_id&place.fields=geo,country,country_code&user.fields=username")
-
-	if err != nil {
-		log.Fatalln(err)
-		return QueryResponse{nil, resp.StatusCode, resp.Status}
-	}
-	if resp.StatusCode != 200 {
-		return QueryResponse{nil, resp.StatusCode, resp.Status}
-	}
-	//We Read the response body on the line below.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-		return QueryResponse{nil, resp.StatusCode, resp.Status}
-	}
-	//Convert the body to type string
-	sb := string(body)
-
-	return QueryResponse{&sb, resp.StatusCode, resp.Status}
-}
-
-func getUser(username string) *UIDLookup {
-	username = url.QueryEscape(username)
-	resp, err := getRequest("https://api.twitter.com/2/users/by/username/" + username)
-
-	if err != nil {
-		log.Fatalln(err)
-		return nil
-	}
-	if resp.StatusCode != 200 {
-		return nil
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-		return nil
-	}
-	var uid UIDLookup
-	err = json.Unmarshal([]byte(string(body)), &uid)
-	if err != nil {
-		panic(err)
-	}
-	return &uid
-}
-
-func getTweetsFromUser(ID string) *ProfileTweets {
-
-	ID = url.QueryEscape(ID)
-	resp, err := getRequest("https://api.twitter.com/2/users/" + ID + "/tweets?max_results=15")
-
-	if err != nil {
-		log.Fatalln(err)
-		return nil
-	}
-	if resp.StatusCode != 200 {
-		return nil
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-		return nil
-	}
-	var tweets ProfileTweets
-	err = json.Unmarshal([]byte(string(body)), &tweets)
-	if err != nil {
-		panic(err)
-	}
-
-	return &tweets
-}
-
-func returnTweetQuery(c *gin.Context, res QueryResponse) {
-	var v interface{}
-	json.Unmarshal([]byte(*res.Body), &v)
-	data := v.(map[string]interface{})
-	c.JSON(http.StatusOK, data)
-}
-
-func returnTweetErr(c *gin.Context, res QueryResponse) {
-	c.JSON(res.Code, res.Status)
-}
-
-func getGhigliottinaSolution() string {
-	user := getUser("quizzettone")
-	if user == nil {
-		return ""
-	}
-	tweets := getTweetsFromUser(user.Data.ID)
-	if tweets == nil {
-		return ""
-	}
-	for i := 0; i < tweets.Meta.ResultCount; i++ {
-		if strings.Contains(tweets.Data[i].Text, "La #parola della #ghigliottina de #leredita di oggi è:") {
-			m := regexp.MustCompile(`La #parola della #ghigliottina de #leredita di oggi è:\s([A-Z]|[a-z])+`)
-			return strings.Trim(m.FindString(tweets.Data[i].Text), "La #parola della #ghigliottina de #leredita di oggi è: ")
-		}
-	}
-	return ""
-}
 
 // TestTwitter godoc
 // @Summary     Test Twitter API
@@ -171,4 +23,35 @@ func TestTwitter(ctx *gin.Context) {
 	} else {
 		returnTweetErr(ctx, resp)
 	}
+}
+
+// GameSolution godoc
+// @Summary     Retrieve game's solution
+// @Tags        games
+// @Produce     json
+// @Param       game	path	string	true	"Game to search"
+// @Success     200
+// @Failure     404	{string}	string  "param GAME not found"
+// @Router      /{game}/solution [get]
+func GameSolution(ctx *gin.Context) {
+	q := ctx.Param("game")
+	fmt.Println(q)
+	if q == "ghigliottina" {
+		sol := getGhigliottinaSolution()
+		if sol != "" && sol != "ERR_USER" && sol != "ERR_TWEETS" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"solution": sol})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": sol})
+		}
+	} else {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "param GAME not found"})
+	}
+}
+
+func InitAPI(v1 *gin.RouterGroup) {
+	v1.GET("/:game/solution", GameSolution)
+
 }
--- twitter/twitter_test.go
+++ twitter/twitter_test.go
@@ -3,14 +3,15 @@ package twitter
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"git.hjkl.gq/team13/birdazzone-api/util"
 )
 
 func TestGetTweetsFromQuery(t *testing.T) {
 	res := getTweetsFromQuery("kalulu")
-	if res.Code != 200 {
-		t.Fatalf("Cannot retrieve GET request from Twitter")
+	if res.Code != http.StatusOK {
+		t.Fatalf("Expected code %d but instead got %d", http.StatusOK, res.Code)
 	}
 }
 
@@ -21,5 +22,45 @@ func TestReturnTweetQuery(t *testing.T) {
 	if util.GetTestingResponseRecorder().Code != http.StatusOK {
 		t.Fatalf("Expected to get status %d but instead got %d", http.StatusOK,
 			util.GetTestingResponseRecorder().Code)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	uid := getUser("quizzettone")
+	if uid == nil {
+		t.Fatal("Error in GetUser")
+	}
+}
+
+func TestGetTweetsFromUser(t *testing.T) {
+	tweets := getTweetsFromUser("1499992669480755204", "")
+	if tweets == nil {
+		t.Fatal("Error in GetTweetsFromUser")
+	}
+}
+
+func TestGetGhigliottinaSolution(t *testing.T) {
+	sol := getGhigliottinaSolution()
+	if sol == "ERR_USER" {
+		t.Fatal("Error in retrieving solution's author")
+	}
+	if sol == "ERR_TWEETS" {
+		t.Fatal("Error in retrieving solution's tweets")
+	}
+	if sol == "" {
+		t.Log("No available solution")
+	}
+}
+
+func TestTimeFormat(t *testing.T) {
+	tm := time.Date(2022, time.September, 10, 18, 59, 00, 00, time.UTC)
+	x := timeFormat(tm)
+	if x != "2022-09-09T18:55:00Z" {
+		t.Fatal("Error in formatting time #1")
+	}
+	tm = time.Date(2022, time.December, 01, 10, 19, 01, 00, time.UTC)
+	x = timeFormat(tm)
+	if x != "2022-11-30T18:55:00Z" {
+		t.Fatal("Error in formatting time #2")
 	}
 }
