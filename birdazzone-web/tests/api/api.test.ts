7185a14b96a790b154363ad79b6d99cf84e941a1
mattia <>
2022-11-12 17:23:37 +0100

    chore

--- birdazzone-web/tests/api/api.test.ts
+++ birdazzone-web/tests/api/api.test.ts
@@ -1,11 +1,14 @@
-import { ApiResponse, ApiManager } from '/src/api/api'
-import { test, assert, expect} from 'vitest'
-import { createRouter, createWebHistory } from 'vue-router'
-import HelloBird from '/src/components/HelloBird.vue'
+import { ApiResponse } from '/src/api/api'
+import { test, assert, expect } from 'vitest'
 
 test('Successfull ApiResponse', () => {
+  // Arrange
   const STATUS_CODE = 200, DATA = 42
+
+  // Act
   let ar = new ApiResponse<number>(STATUS_CODE, DATA, null)
+
+  // Assert
   expect(ar.data).toBe(DATA)
   expect(ar.error).toBeNull()
   expect(ar.statusCode).toBe(STATUS_CODE)
@@ -13,8 +16,13 @@ test('Successfull ApiResponse', () => {
 })
 
 test('Unsuccessfull ApiResponse', () => {
+  // Arrange
   const STATUS_CODE = 404, DATA = "boh", ERROR = "Not found!"
+
+  // Act
   let ar = new ApiResponse<string>(STATUS_CODE, DATA, ERROR)
+
+  // Assert
   expect(ar.data).toBe(DATA)
   expect(ar.error).toBe(ERROR)
   expect(ar.statusCode).toBe(STATUS_CODE)
