aee12aad2e19b62eeb714b863d26a617cd132f3a
Paolo Ceroni <paoloceroni01@gmail.com>
2022-11-16 15:26:49 +0100

    TG-119 #ready-for-test messaggio in caso di lista vuota

--- birdazzone-web/src/components/GuesserList.vue
+++ birdazzone-web/src/components/GuesserList.vue
@@ -6,25 +6,25 @@
 
     const error = ref<boolean> (false)
     const list = ref<Tweet[]>([])
-    
     const props = defineProps<{gameId: number}>()
 
     const fetchList = async () => {
     const resp = await ApiRepository.getListOfGuesser(props.gameId.toString())
-    if (resp.esit) {
-      list.value = resp.data!.entries
-    } else {
-      error.value = true
+      if (resp.esit) {
+        list.value = resp.data!.entries
+      } else {
+        error.value = true
+      }
     }
-  }
-
     onBeforeMount(fetchList)
 </script>
 
 <template>
-    <div>
-
-    <div class="flex flex-col" v-for="(item,index) in list" :key="index">
+  <div>
+    <div v-if="list.length === 0" class="flex flex-1 my-3 p-10 text-white font-semibold" style="flex: 1 1 auto">
+      Nobody was smart enough to guess &#127773;
+    </div>  
+    <div v-else class="flex flex-col" v-for="(item,index) in list" :key="index">
         <GuesserListItem :data="item" :index="index" class="flex flex-1"/>
     </div>
   </div>
