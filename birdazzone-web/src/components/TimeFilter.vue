f0925cdecd8b47ccf9edcc7346f2b0407901476a
Fedegri <federica.grisendi@studio.unibo.it>
2022-11-18 19:50:51 +0100

    TG-118 #in-progress 180' component creation and filtering research

---
+++ birdazzone-web/src/components/TimeFilter.vue
@@ -0,0 +1,20 @@
+<script setup lang=ts>
+import { ref } from 'vue'
+
+const error = ref<boolean>(false)
+
+</script>
+
+<template>
+  <div v-if="error">
+    <ErrorWidget />
+  </div>
+
+  <div class="bg-foreground font-semibold text-lg rounded-lg m-4 p-4 place-self-center">
+
+    <!--<div v-if="loading">
+      <semipolar-spinner :animation-duration="2000" :size="35" color="#1eb980" />
+    </div>-->
+
+  </div>
+</template>
--- birdazzone-web/src/views/TvGamesView.vue
+++ birdazzone-web/src/views/TvGamesView.vue
@@ -3,6 +3,7 @@ import { onBeforeMount, ref } from 'vue'
 import ApiRepository from '@/api/api-repository';
 import type { TvGame } from '@/api/interfaces/tv-game';
 import ErrorWidget from '../components/ErrorWidget.vue';
+import TimeFilter from '../components/TimeFilter.vue';
 
 const error = ref<boolean>(false)
 const games = ref<TvGame[]>([])
@@ -26,6 +27,7 @@ onBeforeMount(fetchTvGames)
   </div>
   <div v-else class="text-center">
     <h1 class="text-4xl font-black text-lgreen">TV GAMES</h1>
+    <TimeFilter />
   </div>
   <div class="flex flex-col justify-evenly">
     <div v-for="game in games">
