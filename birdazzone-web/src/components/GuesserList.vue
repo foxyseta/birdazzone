65414bc2e68ad0c4678686af7ab1889bb6ad3db8
Fedegri <federica.grisendi@studio.unibo.it>
2022-11-19 18:48:07 +0100

    TG-118 120' beginning of the implementation of the front part filters

--- birdazzone-web/src/components/GuesserList.vue
+++ birdazzone-web/src/components/GuesserList.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-    import GuesserListItem from '@/components/GuesserListItem.vue'
-    import ApiRepository from '@/api/api-repository';
-    import {onBeforeMount, ref} from 'vue'
-    import type { Tweet } from '@/api/interfaces/tweet';
+  import GuesserListItem from '@/components/GuesserListItem.vue'
+  import ApiRepository from '@/api/api-repository';
+  import {onBeforeMount, ref} from 'vue'
+  import type { Tweet } from '@/api/interfaces/tweet';
 
-    const error = ref<boolean> (false)
-    const list = ref<Tweet[]>([])
-    const props = defineProps<{gameId: number}>()
+  const error = ref<boolean> (false)
+  const list = ref<Tweet[]>([])
+  const props = defineProps<{gameId: number}>()
 
-    const fetchList = async () => {
-    const resp = await ApiRepository.getListOfGuesser(props.gameId.toString())
-      if (resp.esit) {
-        list.value = resp.data!.entries
-      } else {
-        error.value = true
-      }
+  const fetchList = async () => {
+  const resp = await ApiRepository.getListOfGuesser(props.gameId.toString())
+    if (resp.esit) {
+      list.value = resp.data!.entries
+    } else {
+      error.value = true
     }
-    onBeforeMount(fetchList)
+  }
+  onBeforeMount(fetchList)
 </script>
 
 <template>
