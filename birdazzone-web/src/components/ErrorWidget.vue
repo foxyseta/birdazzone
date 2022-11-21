17ab190261db5afb251cc0d3faee635b4f06ca77
mattia <>
2022-11-21 17:38:36 +0100

    fix: dead.png

--- birdazzone-web/src/components/ErrorWidget.vue
+++ birdazzone-web/src/components/ErrorWidget.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-5">
-    <img src="dead.png" alt="error" class="m-4" />
+    <img src="/dead.png" alt="error" class="m-4" />
     <p class="text-xl font-black text-white text-center"> ups something went wrong!</p>
     <p class="text-l font-bold text-white text-center"> check your internet access</p>
   </div>
