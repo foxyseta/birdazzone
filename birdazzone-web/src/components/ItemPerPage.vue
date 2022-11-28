4cba51f1b0e25c9de5cbda5e88cf2ce68fd9cc30
Paolo Ceroni <paoloceroni01@gmail.com>
2022-11-28 20:00:53 +0100

    TG-105 sistemato bug .value

--- birdazzone-web/src/components/ItemPerPage.vue
+++ birdazzone-web/src/components/ItemPerPage.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-    const propss = defineProps<{itemPerPage:number}>()
+    const props = defineProps<{itemPerPage:number}>()
     const options = [5,10,15,20]
-    function log(value: any){
-        console.log(value)
-    }
 </script>
 
 <template>
     <div class="flex flex-row p-2 rounded-lg text-lgray justify-center" style="width:13rem;flex: 0 1 auto;border: 2px solid #1eb980">
         <label for="cards" class="mr-2">Twitters per page:</label>
-        <select name="cards" id="cards" class="flex justify-center bg-background text-white" @change="(value)=>{$emit('changeItemPage',value.target?.value)}" type="dropdown" style="flex:0 0 0; outline: 0px">
+        <select name="cards" id="cards" class="flex justify-center bg-background text-white" @change="(value)=>{
+            //@ts-ignore
+            $emit('changeItemPage',value.target?.value)}" type="dropdown" style="flex:0 0 0; outline: 0px">
             <option :selected="itemPerPage===5" value="5" >5</option>
             <option :selected="itemPerPage===10" value="10">10</option>
             <option :selected="itemPerPage===15" value="15">15</option>
