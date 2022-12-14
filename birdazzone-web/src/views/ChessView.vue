ca9f01b473e5486b8bbf73977b9650608f84da04
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-14 10:18:37 +0100

    TG-151 beautify

--- birdazzone-web/src/views/ChessView.vue
+++ birdazzone-web/src/views/ChessView.vue
@@ -6,7 +6,7 @@ import BirdazzoneButton from '../components/BirdazzoneButton.vue';
     <div class="flex">
       <a
         class="flex flex-center"
-        href="https://twitter.com/intent/tweet?text=%23birdchess%20Quale%20mossa%20mi%20consigliate%3F&hashtags=chess%2Ctwitter%2Cbirdchess"
+        href="https://twitter.com/intent/tweet?text=Quale%20mossa%20mi%20consigliate%3F&hashtags=birdchess%2Cchess%2Chelp%2Cbirdazzone"
       >
         <BirdazzoneButton :text="'Ask for some suggestions about a chess match'" :active="true"
           >Ask for some suggestions about a chess match</BirdazzoneButton
