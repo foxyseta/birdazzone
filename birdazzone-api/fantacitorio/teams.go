67f913ec23d60ba740b0001a4deb249dc33a1faa
Gabriele Crestanello <gabriele.crestanello@studio.unibo.it>
2022-12-14 10:14:30 +0100

    TG-178 makeFantaTeam

--- birdazzone-api/fantacitorio/teams.go
+++ birdazzone-api/fantacitorio/teams.go
@@ -8,26 +8,48 @@ import (
 	"git.hjkl.gq/team13/birdazzone-api/twitter"
 )
 
-func teamsFromTwitter(username string, hasName bool) ([]string, model.Error) {
-	ret := []string{}
-	search := "#fantacitorio has:media"
+func teamsFromTwitter(username string, hasName bool) ([]model.FantaTeam, model.Error) {
+	search := "#fantacitorio has:media -is:retweet"
 	if hasName {
 		if username == "" || strings.Contains(username, " ") {
-			return ret, model.Error{Code: http.StatusBadRequest, Message: "incorrect syntax for a username"}
+			return []model.FantaTeam{}, model.Error{Code: http.StatusBadRequest, Message: "incorrect syntax for a username"}
 		}
 		search += " from:" + username
 	}
 	result, err := twitter.GetManyRecentTweetsFromQuery(search, "", "")
 	if err != nil {
-		return ret, model.Error{Code: http.StatusInternalServerError, Message: err.Error()}
+		return []model.FantaTeam{}, model.Error{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 	if result.Meta.ResultCount == 0 {
-		return ret, model.Error{Code: http.StatusNotFound, Message: "no team with such username"}
+		return []model.FantaTeam{}, model.Error{Code: http.StatusNotFound, Message: "no team with such username"}
 	}
-	for _, m := range result.Includes.Medias {
-		if m.Height == 512 && m.Width == 1024 {
-			ret = append(ret, m.URL)
+	ret := makeFantaTeam(result)
+	return ret, model.Error{}
+}
+
+func makeFantaTeam(tweets *twitter.ProfileTweets) []model.FantaTeam {
+	var ret []model.FantaTeam
+	for _, media := range tweets.Includes.Medias {
+		if media.Width == 1024 && media.Height == 512 {
+			for _, data := range tweets.Data {
+				if len(data.Attachments.MediaKeys) > 0 && media.MediaKey == data.Attachments.MediaKeys[0] {
+					for _, user := range tweets.Includes.Users {
+						if data.AuthorId == user.ID {
+							ret = append(ret,
+								model.FantaTeam{
+									Username:        user.Username,
+									ProfileImageUrl: user.ProfileImageUrl,
+									PostUrl:         "https://twitter.com/anyuser/status/" + data.ID,
+									ImageUrl:        media.URL,
+								})
+
+							break
+						}
+					}
+					break
+				}
+			}
 		}
 	}
-	return ret, model.Error{}
+	return ret
 }
