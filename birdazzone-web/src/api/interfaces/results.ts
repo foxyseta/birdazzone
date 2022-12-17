30a4c6fa3c0a4b164379cbacb9e56ef506774960
mattia <>
2022-12-17 18:53:54 +0100

    feat: linechart 4 tvgame

--- birdazzone-web/src/api/interfaces/results.ts
+++ birdazzone-web/src/api/interfaces/results.ts
@@ -1,4 +1,5 @@
 export interface Results {
+  label: string
   positives: number;
   negatives: number;
 }
--- birdazzone-web/src/components/FilterHistogram.vue
+++ birdazzone-web/src/components/FilterHistogram.vue
@@ -140,7 +140,7 @@ export default defineComponent({
       >
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
       </svg>
-      filter
+      Date filter
     </button>
   </div>
 
