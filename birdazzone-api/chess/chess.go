a628505a43c9679087b377b57c7b094249f75480
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-17 21:05:07 +0100

    TG-160 #in-progress notation.go setup

--- birdazzone-api/chess/chess.go
+++ birdazzone-api/chess/chess.go
@@ -16,7 +16,6 @@ func ChessGroup(group *gin.RouterGroup) {
 // @Produce json
 // @Param   tweet path     string      true "#birdchess tweet ID to query for"
 // @Success 200   {array}  model.Chart "A chart whose values are the suggested moves"
-// @Failure 400   {object} model.Error "The specified tweet is no #birdchess tweet"
 // @Failure 404   {object} model.Error "No tweet with the given ID found"
 // @Router  /chess/{tweet} [get]
 func getChessMoves(ctx *gin.Context) {
