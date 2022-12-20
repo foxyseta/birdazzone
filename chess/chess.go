e8895669b2fa1b69025f10f46a2d1e4e47201158
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-20 04:15:33 +0100

    Update chess API docs

--- chess/chess.go
+++ chess/chess.go
@@ -7,17 +7,21 @@ import (
 )
 
 func ChessGroup(group *gin.RouterGroup) {
-	group.GET("/:tweet", getChessMoves)
+	group.GET("/:user/:game/:turn", getChessMoves)
 }
 
 // getChessMoves godoc
 // @Summary Get all suggested moves for a given #birdchess tweet
 // @Tags    chess
 // @Produce json
-// @Param   tweet path     string      true "#birdchess tweet ID to query for"
-// @Success 200   {array}  model.Chart "A chart whose values are the suggested moves"
-// @Failure 404   {object} model.Error "No tweet with the given ID found"
-// @Router  /chess/{tweet} [get]
+// @Param   user path     string      true "player 1's username"
+// @Param   game path     string      true "game identifier, i.e. its starting instant" Format(date-time)
+// @Param   turn path     int         true "second player's turn number"
+// @Success 200  {string} string      "The second player's move"
+// @Success 204  {string} string      "No one has played yet"
+// @Failure 400  {object} model.Error "game > now or turn < 0"
+// @Failure 404  {object} model.Error "No user or no post found"
+// @Router  /chess/{user}/{game}/{turn} [get]
 func getChessMoves(ctx *gin.Context) {
 	ctx.JSON(http.StatusNotImplemented, nil)
 }
