aeee5273be5a5fe7c27734f3abd41262bbcbf9a4
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-21 17:19:49 +0100

    Incorporate chess

--- birdazzone-web/src/views/ChessView.vue
+++ birdazzone-web/src/views/ChessView.vue
@@ -1,19 +1,58 @@
 <script setup lang="ts">
 import BirdazzoneButton from '../components/BirdazzoneButton.vue';
+import { ref } from 'vue';
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+import type { ChessboardAPI, BoardConfig } from 'vue3-chessboard';
+
+const boardAPI = ref<ChessboardAPI>();
+const boardConfig: BoardConfig = {
+  coordinates: false,
+  autoCastle: false,
+  events: {
+    move: updateIntent
+  }
+};
+const intent = ref("");
+
+function updateIntent() {
+  intent.value =
+      "https://twitter.com/intent/tweet?url=https%3A%2F%2Ffen2image.chessvision.ai%2F" +
+      encodeURIComponent(/*boardAPI?.value?.board.getFen() ??*/ "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") +
+      "&text=Your%20move.%20Please%20use%20the%20%22a1b2%22%20format.%20This%20is%20a%20majority%20vote."
+}
+
+updateIntent();
+
+function handleCheckmate(isMated: string) {
+  if (isMated === 'w') {
+    alert('Black wins!');
+  } else {
+    alert('White wins!');
+  }
+}
+
 </script>
 <template>
-  <main class="flex justify-center items-center" style="flex: 1 1 auto">
-    <div class="flex">
+  <div class="flex flex-col place-content-center flex-initial justify-start place-items-center w-full mb-10 h-screen">
+    <div class="shadow-4xl rounded-lg text-white bg-lgreen text-4xl font-semibold p-3 my-12 m-3">CHESS</div>
+    <div>
       <a
         class="flex flex-center"
-        href="https://twitter.com/intent/tweet?text=Quale%20mossa%20mi%20consigliate%3F&hashtags=birdchess%2Cchess%2Chelp%2Cbirdazzone"
+        :href="intent"
         target="_blank"
         rel="noopener noreferrer"
       >
         <BirdazzoneButton :text="'Ask for some suggestions about a chess match'" :active="true"
-          >Ask for some suggestions about a chess match</BirdazzoneButton
+          >Ask the scrum for a move</BirdazzoneButton
         >
       </a>
     </div>
-  </main>
+    <chessboard />
+    <TheChessboard
+      :board-config="boardConfig"
+      @board-created="(api) => (boardAPI = api)"
+      @checkmate="handleCheckmate"
+    />
+  </div>
 </template>
