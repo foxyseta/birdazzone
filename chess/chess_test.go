cd4687b8822f9cc386552e68facff3ad6e8bf2f4
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-22 04:32:48 +0100

    TG-214 Test some chess utility functions

--- chess/chess_test.go
+++ chess/chess_test.go
@@ -1,8 +1,35 @@
 package chess
 
 import (
+	"net/http"
 	"testing"
+
+	"git.hjkl.gq/team13/birdazzone-api/twitter"
 )
 
-func TestGetTvGames(t *testing.T) {
+func TestUncheckedGetCheckMoveWrongDate(t *testing.T) {
+	_, err := uncheckedGetCheckMove("anything", "2017-09-12T20:00:00Z", 2)
+	if err == nil {
+		t.Fatal("Error expected")
+	}
+	if err.Code != http.StatusNotFound {
+		t.Fatalf("Got %d, but expected 404", err.Code)
+	}
+}
+
+func TestUncheckedGetCheckMoveNoQuotes(t *testing.T) {
+	_, err := uncheckedGetCheckMove("twitter", "2017-09-12T20:00:00Z", 2)
+	if err == nil {
+		t.Fatal("Error expected")
+	}
+	if err.Code != http.StatusNotFound {
+		t.Fatalf("Got %d, but expected 404", err.Code)
+	}
+}
+
+func TestMostPopularChessMove(t *testing.T) {
+	answer := mostPopularChessMove(twitter.ProfileTweet{ID: "1605757925951242240"})
+	if answer != "e7e6" {
+		t.Fatalf("Expected 'e7e6'. Found '%s' instead", answer)
+	}
 }
