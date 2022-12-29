b318df25615363e97a3645a1af748010970a6f88
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2022-12-29 22:00:16 +0100

    Rm web

--- birdazzone-web/src/api/interfaces/solution.ts
+++
@@ -1,3 +0,0 @@
-export interface Solution {
-  solution: string;
-}
--- birdazzone-web/tests/App.test.ts
+++
@@ -1,6 +0,0 @@
-import App from '/src/App.vue';
-import { test, expect } from 'vitest';
-
-test('App is truthy', () => {
-  expect(App).toBeTruthy();
-});
